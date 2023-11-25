db1c796af340bebf0bbb90a47170f44198cdef15
jpb <jakub.baranski.33@gmail.com>
2023-11-25 09:17:52 +0000

    Begin adding web-based download service to get app details

--- AppStore/Views/UTEUWPPage.xaml.cs
+++ AppStore/Views/UTEUWPPage.xaml.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Runtime.InteropServices.WindowsRuntime;
+using CommunityToolkit.WinUI.Notifications;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -12,6 +14,13 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Networking.BackgroundTransfer;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.System;
+using Windows.Foundation;
+using Windows.Networking.BackgroundTransfer;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -25,6 +34,16 @@ public sealed partial class UTEUWPPage : Page
     public UTEUWPPage()
     {
         this.InitializeComponent();
+        Uri sourcetext = new Uri("https://github.com/jpbandroid/AppStore-Resources/raw/main/UltraTextEdit%20UWP/dev_version.txt");
+        WebClient client = new WebClient();
+        client.DownloadFile(sourcetext, Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/UTE-dev-version.txt");
+        // Use it on your StreamReader
+        var filename = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "/UTE-dev-version.txt";
+        StreamReader sr = new StreamReader(filename);
+        versiontext.Text = "version " + sr.ReadToEnd();
+        tdowntext.Text = sourcetext.ToString();
+        tstortext.Text = filename.ToString();
+        mstortext.Text = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
     }
 
     private void hyperlinkclick(object sender, RoutedEventArgs e)
@@ -32,8 +51,19 @@ public sealed partial class UTEUWPPage : Page
 
     }
 
-    private void download(object sender, RoutedEventArgs e)
+    private async void download(object sender, RoutedEventArgs e)
     {
+        string name = Environment.UserName;
+        using var client = new HttpClient();
+        using var s = await client.GetStreamAsync("https://github.com/jpbandroid/AppStore-Resources/raw/main/UltraTextEdit%20UWP/UltraTextEdit%20UWP_10.0.25915.1000_x86_x64_arm_arm64.msixbundle");
+        using var fs = new FileStream("C:\\Users\\"+name+"\\Downloads\\UTEUWP.msixbundle", FileMode.OpenOrCreate);
+        await s.CopyToAsync(fs);
+        new ToastContentBuilder()
+            .AddArgument("action", "viewConversation")
+            .AddArgument("conversationId", 9813)
+            .AddText("UltraTextEdit UWP download complete")
+            .AddText("Take a look!")
+            .Show(); // Not seeing the Show() method? Make sure you have version 7.0, and if you're using .NET 5, your TFM must be net5.0-windows10.0.17763.0 or greater
 
     }
 }
